98521a84b462e81335638fb170f0f8f0413f96fb
syed-ashraf09 <122000601+syed-ashraf09@users.noreply.github.com>
2023-07-30 18:50:16 +0530

    updating preprocessor.py file

--- preprocessor.py
+++ preprocessor.py
@@ -3,27 +3,39 @@ import pandas as pd
 
 
 def preprocess(data):
+    flag = False
     pattern = "\d{2}/\d{2}/\d{2,4},\s\d{1,2}:\d{2}\s[ap]m\s-\s"
     messages = re.split(pattern, data)[1:]
+    if len(messages) is 0:
+        pattern = "\d{2}/\d{2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s"
+        messages = re.split(pattern, data)[1:]
+        flag = True
     dates = re.findall(pattern, data)
     for i in range(len(dates)):
         dates[i] = dates[i].replace("\u202f", " ")
-    for i in range(len(dates)):
-        x = dates[i].split()
-        if len(x[0]) > 9:
-            x[0] = x[0][0:6] + x[0][8:]
-        hour, minute = x[1].strip().split(':')
-        if x[2] == 'pm' and int(hour) != 12:
-            hour = str(int(hour) + 12)
-        elif x[2] == 'am':
-            if int(hour) == 12:
-                hour = '00'
-            elif int(hour) < 10:
-                hour = '0' + hour
+    if not flag:
+        for i in range(len(dates)):
+            x = dates[i].split()
+            if len(x[0]) > 9:
+                x[0] = x[0][0:6] + x[0][8:]
+            hour, minute = x[1].strip().split(':')
+            if x[2] == 'pm' and int(hour) != 12:
+                hour = str(int(hour) + 12)
+            elif x[2] == 'am':
+                if int(hour) == 12:
+                    hour = '00'
+                elif int(hour) < 10:
+                    hour = '0' + hour
 
-        x[1] = ':'.join([hour, minute])
-        x.pop(2)
-        dates[i] = ' '.join(x)
+            x[1] = ':'.join([hour, minute])
+            x.pop(2)
+            dates[i] = ' '.join(x)
+    if flag:
+        li=[]
+        for i in dates:
+            li.append(i[:-1])
+        dates = li
+
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M -')
     df.rename(columns={'message_date': 'date'}, inplace=True)
